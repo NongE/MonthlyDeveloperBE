a995cfa4b4e5e33675e6f689410d40ec452bcf71
NongE <hsb9504@gmail.com>
2021-12-09 23:12:40 +0900

    Oauth 코드 정리 및 파일 분리화
    - Blueprint 대신 Namespace 사용
    - flask-restx 에 맞춰 수정

--- flask_monthly_developer_backend/app.py
+++ flask_monthly_developer_backend/app.py
@@ -3,11 +3,10 @@ from flask_restx import Api
 
 import os
 
-from controller import recruit_board
-from controller.authentication import oauth, auth
+from controller import recruit_board, authentication
+from controller.authentication import oauth
 from config import config
 from config import db_connector
-
 
 def create_env():
     app = Flask(__name__)
@@ -21,9 +20,10 @@ def create_env():
     oauth.init_app(app)
 
     api = Api(app, version=1.0, title="flask_env", description="flask_env_test")
-    app.register_blueprint(auth, url_prefix='/')
+    # app.register_blueprint(auth, url_prefix='/')
 
     # namespace를 추가합니다.
     api.add_namespace(recruit_board.recruit_ns, '/recruit')
+    api.add_namespace(authentication.auth_ns, '/login')
 
     return app
--- flask_monthly_developer_backend/controller/authentication.py
+++ flask_monthly_developer_backend/controller/authentication.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, url_for
+from flask import url_for
+from flask_restx import Resource, Namespace
 from authlib.integrations.flask_client import OAuth
 from config.config import GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET
+
 oauth = OAuth()
 
-auth = Blueprint('auth', __name__)
+auth_ns = Namespace("Github Oauth", description="Github Oauth 로그인")
 
 github = oauth.register(
     name='github',
@@ -17,26 +19,27 @@ github = oauth.register(
     client_kwargs={'scope': 'user:email'},
 )
 
-
 # Github login route
-@auth.route('/login/github')
-def github_login():
-    github_ = oauth.create_client('github')
-    redirect_uri = url_for('auth.github_authorize', _external=True)
-    return github_.authorize_redirect(redirect_uri)
+@auth_ns.route('/github', methods=['GET'])
+class GithubLogin(Resource):
+    def get(self):
+        github_ = oauth.create_client('github')
+        redirect_uri = url_for('Github Oauth_github_authorize', _external=True)
+        return github_.authorize_redirect(redirect_uri)
 
 
 # Github authorize route
-@auth.route('/login/github/authorize')
-def github_authorize():
+@auth_ns.route('/github/authorize', methods=['GET'])
+class GithubAuthorize(Resource):
+    def get(self):
 
-    # github_ = oauth.create_client('github')
-    # access_token = github_.authorize_access_token()['access_token']
-    # 위와 같은 식으로도 사용 가능
+        # github_ = oauth.create_client('github')
+        # access_token = github_.authorize_access_token()['access_token']
+        # 위와 같은 식으로도 사용 가능
 
-    access_token = oauth.github.authorize_access_token()['access_token']
-    resp = oauth.github.get('user').json()
+        access_token = oauth.github.authorize_access_token()['access_token']
+        resp = oauth.github.get('user').json()
 
-    user_ID, email = resp['login'], resp['email']
+        user_ID, email = resp['login'], resp['email']
 
-    return f"You are successfully signed in using github, user_ID : {user_ID} / E-mail : {email}"
+        return f"You are successfully signed in using github, user_ID : {user_ID} / E-mail : {email}"
